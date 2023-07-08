f9b4464e9f095d124609a22328acec396b5e2b09
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-07-08 22:39:32 +0300

    set initial page state to 'home'

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,6 +1,11 @@
+import { useTheme } from "@mui/material"
+import { useState } from "react"
 
 
 const Navbar = () => {
+  const { palette } = useTheme()
+  const [selected, setSelected] = useState("home")
+
   return (
     <div>Navbar</div>
   )
