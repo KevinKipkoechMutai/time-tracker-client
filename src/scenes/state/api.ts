0fc5c9c6693256f4e84c5349d7d9798ddbf8d7c9
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-07-09 09:46:04 +0300

    create api fetch base

---
+++ src/scenes/state/api.ts
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { GetTasksResponse } from "./types"
+
+export const api = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:1337" }),
+    reducerPath: "main",
+    tagTypes: ["Tasks"],
+    endpoints: (build) => ({
+        getTasks: build.query<Array<GetTasksResponse>, void>({
+            query: () => "task/tasks/",
+            providesTags: ["Tasks"]
+        })
+    })
+})
